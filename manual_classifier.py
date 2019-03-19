f9ec7fdeb8600e78ac9d9d5b86dacc88f5f29513
danmargera <danterenzi@hotmail.com>
2019-03-19 10:17:59 -0300

    Implemented manual classifier script

---
+++ manual_classifier.py
@@ -0,0 +1,39 @@
+import json
+import msvcrt
+import sys
+
+def classify(tweet_text):
+    print('--------------------------------')
+    print(tweet_text)
+    print('Positive(P) Negative(N) Neutral(Any Key)')
+    input = msvcrt.getch()
+    if (input == b'p' or input == b'P'):
+        return {
+            "text" : tweet_text,
+            "sentiment" : 1
+        }
+    elif (input == b'n' or input == b'N'):
+        return {
+            "text" : tweet_text,
+            "sentiment" : -1
+        }
+    else:
+        return {
+            "text" : tweet_text,
+            "sentiment" : 0
+        }
+
+if __name__ == "__main__":
+    try:
+        filename = sys.argv[1]
+    except IndexError as error:
+        print('Error! No file passed as argument')
+        print('Usage: manual_classifier.py your_data_file.json')
+        exit()
+
+    data = json.loads(open(filename).read())
+    classified_data = dict()
+    for k,v in data.items():
+        classified_data.update({k : classify(v)})
+
+    print(json.dumps(classified_data, indent=4))
