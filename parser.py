5ea8ca5283d05c32ebe24a25abb69c63e77261fa
Felipe Megale <felipe.megale17@gmail.com>
2019-03-19 21:12:51 -0300

    Update parser.py to take in multiple twitter_data files at once

--- parser.py
+++ parser.py
@@ -1,21 +1,21 @@
 import json
 import sys
 
-raw_file_name = sys.argv[1]
-
+file_names = sys.argv[1:]
 tweet_dict = dict()
 
-with open(raw_file_name, 'r') as data:
-    with open("out.txt", 'w') as out_file:
-        for line in data:
-            if line != '\n':
-                tweet = json.loads(line)
-                tweet_id = tweet['id']
-                try:
-                    full_text = tweet['extended_tweet']['full_text']
-                except:
-                    full_text = tweet['text']
-                finally:
-                    # print((tweet_id +": "+ full_text).encode('utf-8'))
-                    tweet_dict.update({tweet_id: full_text})
-        out_file.write(json.dumps(tweet_dict, indent=4))
+for file_name in file_names:
+    with open(file_name, 'r') as data:
+        with open("out.txt", 'a') as out_file:
+            for line in data:
+                if line != '\n':
+                    tweet = json.loads(line)
+                    tweet_id = tweet['id']
+                    try:
+                        full_text = tweet['extended_tweet']['full_text']
+                    except:
+                        full_text = tweet['text']
+                    finally:
+                        # print((tweet_id +": "+ full_text).encode('utf-8'))
+                        tweet_dict.update({tweet_id: full_text})
+            out_file.write(json.dumps(tweet_dict, indent=4))
