362b6f09e3394fb35642e68f96ca6ee408dc7310
Felipe Megale <felipe.megale17@gmail.com>
2019-03-19 22:13:35 -0300

    fixed parser

--- parser.py
+++ parser.py
@@ -3,19 +3,27 @@ import sys
 
 file_names = sys.argv[1:]
 tweet_dict = dict()
+limit = str()
 
 for file_name in file_names:
     with open(file_name, 'r') as data:
         with open("out.txt", 'a') as out_file:
             for line in data:
-                if line != '\n':
+                if line != '\n' and line != '\r':
                     tweet = json.loads(line)
-                    tweet_id = tweet['id']
                     try:
+                        tweet_id = tweet['id']
                         full_text = tweet['extended_tweet']['full_text']
                     except:
-                        full_text = tweet['text']
+                        # print(tweet['id'])
+                        try:
+                            full_text = tweet['text']
+                        except:
+                            limit = tweet['limit']
                     finally:
                         # print((tweet_id +": "+ full_text).encode('utf-8'))
-                        tweet_dict.update({tweet_id: full_text})
+                        if not limit:
+                            tweet_dict.update({tweet_id: full_text})
+                        else:
+                            limit = ''
             out_file.write(json.dumps(tweet_dict, indent=4))
