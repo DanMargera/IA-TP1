a86a43a22bebc7255a97d416c27420d6018acebb
DanMargera <danterenzi@hotmail.com>
2019-04-27 12:35:21 -0300

    Adding preprocessor for tweet text

---
+++ preprocess.py
@@ -0,0 +1,51 @@
+import re
+
+
+def replaceURL(text):
+    """ Replaces url address with "url" """
+    text = re.sub(r'((www\.[^\s]+)|(https?://[^\s]+))','url',text)
+    text = re.sub(r'#([^\s]+)', r'\1', text)
+    return text
+
+def removeUnicode(text):
+    """ Removes unicode strings like "\u002c" and "x96" """
+    text = re.sub(r'(\\u[0-9A-Fa-f]+)',r'', text)       
+    text = re.sub(r'[^\x00-\x7f]',r'',text)
+    return text
+
+def replaceAtUser(text):
+    """ Replaces "@user" with "" """
+    text = re.sub(r'@[^\s]+','',text)
+    return text
+
+def removeHashtagInFrontOfWord(text):
+    """ Removes hastag in front of a word """
+    text = re.sub(r'#([^\s]+)', r'\1', text)
+    return text
+
+def removeHashtag(text):
+    """ Removes hastags """
+    text = re.sub(r'#([^\s]+)', '', text)
+    return text
+
+def replaceMultiExclamationMark(text):
+    """ Replaces repetitions of exlamation marks """
+    text = re.sub(r"(\!)\1+", '', text)
+    return text
+
+def removeEmoticons(text):
+    """ Removes emoticons from text """
+    text = re.sub(r':\)|;\)|:-\)|\(-:|:-D|=D|:P|xD|X-p|\^\^|:-*|\^\.\^|\^\-\^|\^\_\^|\,-\)|\)-:|:\'\(|:\(|:-\(|:\S|T\.T|\.\_\.|:<|:-\S|:-<|\*\-\*|:O|=O|=\-O|O\.o|XO|O\_O|:-\@|=/|:/|X\-\(|>\.<|>=\(|D:', '', text)
+    return text
+
+""" Replaces contractions from a string to their equivalents """
+contraction_patterns = [ (r'won\'t', 'will not'), (r'can\'t', 'cannot'), (r'i\'m', 'i am'), (r'ain\'t', 'is not'), (r'(\w+)\'ll', '\g<1> will'), (r'(\w+)n\'t', '\g<1> not'),
+                         (r'(\w+)\'ve', '\g<1> have'), (r'(\w+)\'s', '\g<1> is'), (r'(\w+)\'re', '\g<1> are'), (r'(\w+)\'d', '\g<1> would'), (r'&', 'and'), (r'dammit', 'damn it'), (r'dont', 'do not'), (r'wont', 'will not') ]
+def replaceContraction(text):
+    patterns = [(re.compile(regex), repl) for (regex, repl) in contraction_patterns]
+    for (pattern, repl) in patterns:
+        (text, count) = re.subn(pattern, repl, text)
+    return text
+
+def process(text):
+    return replaceURL(replaceContraction(replaceAtUser(removeHashtag(replaceMultiExclamationMark(text)))))
